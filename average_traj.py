cb31f68cf1a4fdbb182511ba7b6453e6eabc79ca
GAST Nicolas <nicolas.gast@inria.fr>
2018-05-25 09:49:31 +0200

    cleaning of module to be usable with relative paths

--- average_traj.py
+++ average_traj.py
@@ -4,10 +4,12 @@ import pandas as pd
 from time import time
 
 os.system('make simulate_JSQ')
+path = os.path.abspath(__file__)
+dir_path = os.path.dirname(path)
 
 def simulateAverageTraj(N,rho,initial_number_of_jobs,nb_samples):
-    fileName = 'jsqD_simulate/traj/averageTraj_N{}_r{}_init{}.npz'.format(
-        N,int(rho*100),int(initial_number_of_jobs*10))
+    fileName = '{}/traj/averageTraj_N{}_r{}_init{}.npz'.format(
+        dir_path,N,int(rho*100),int(initial_number_of_jobs*10))
     t = time()
     if os.path.exists(fileName):
         loadedFile=np.load(fileName)
@@ -22,9 +24,9 @@ def simulateAverageTraj(N,rho,initial_number_of_jobs,nb_samples):
         nb_samples_already_computed=0
         x=np.zeros((N*300,20))
     for i in range(nb_samples_already_computed,nb_samples):
-        os.system('jsqD_simulate/simulate_JSQ N{} r{} t{} > jsqD_simulate/traj/tmpFile'.format(
-            N,rho,initial_number_of_jobs))
-        y=np.array(pd.read_csv('jsqD_simulate/traj/tmpFile',sep=' ',dtype=np.float64))[:,:-1]
+        os.system('{0}/simulate_JSQ N{1} r{2} t{3} > {0}/traj/tmpFile'.format(
+            dir_path,N,rho,initial_number_of_jobs))
+        y=np.array(pd.read_csv('{}/traj/tmpFile'.format(dir_path),sep=' ',dtype=np.float64))[:,:-1]
         x+=y
         if ((i+1) % 100==0):
             print('\rCompleted:{}/{}, rho={},N={}, estimated remaining time={:.0f}sec'.format(
@@ -36,10 +38,17 @@ def simulateAverageTraj(N,rho,initial_number_of_jobs,nb_samples):
     np.savez(fileName,x=x,nb_samples=max(nb_samples,nb_samples_already_computed))
 
 
-def loadTransientSimu(N,rho,initial_number_of_jobs):
-    fileName='jsqD_simulate/traj/averageTraj_N{}_r{}_init{}.npz'.format(
-        N,int(rho*100),int(10*initial_number_of_jobs))
+def loadTransientSimu(N,rho,initial_number_of_jobs,nb_samples=100):
+    fileName='{}/traj/averageTraj_N{}_r{}_init{}.npz'.format(
+        dir_path,N,int(rho*100),int(10*initial_number_of_jobs))
+    if not os.path.exists(fileName) :
+        print('No data file found : we need to simulate')
+        simulateAverageTraj(N,rho,initial_number_of_jobs,nb_samples)
     myData = np.load(fileName)
+    if myData['nb_samples'] < nb_samples:
+        simulateAverageTraj(N,rho,initial_number_of_jobs,nb_samples)
+        myData = np.load(fileName)
+    
     Y = myData['x']/myData['nb_samples']
     Tsimu = np.arange(0,300*N)/(N*(1+rho))
     print('average over ',myData['nb_samples'],'simulations')
